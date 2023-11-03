a7531955af6d24e65accbf490a3ee4922c76aa23
John Yu <johnyu.programmer@gmail.com>
2023-11-02 17:48:56 -0700

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import "./App.css";
 import { HikingCollection } from "./ui-components";
 import { MapView } from "@aws-amplify/ui-react-geo";
 import "@aws-amplify/ui-react-geo/styles.css";
+import { DataStore } from "aws-amplify";
+import { useState, useEffect } from "react";
+import { Hike } from "./models";
+import { Marker } from "react-map-gl";
+
 function App() {
+  const [hikes, setHikes] = useState([]);
+
+  useEffect(() => {
+    const getHikes = async () => {
+      const hikes = await DataStore.query(Hike);
+      setHikes(hikes);
+    };
+
+    getHikes();
+  }, []);
+
   return (
     <div className="App">
       <HikingCollection />
@@ -10,9 +26,13 @@ function App() {
         initialViewState={{
           latitude: 33.743281,
           longitude: -117.868986,
-          zoom: 11,
+          zoom: 14,
         }}
-      />
+      >
+        {hikes.map((hike) => {
+          <Marker latitude={hike.lat} longitude={hike.long} key={hike.id} />;
+        })}
+      </MapView>
     </div>
   );
 }
