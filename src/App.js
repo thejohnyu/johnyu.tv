26d0bfffcaf785d8a8b63d37d4e04f979f9a6e0d
John Yu <johnyu.programmer@gmail.com>
2023-11-03 23:41:48 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -6,42 +6,36 @@ import { useState, useEffect } from "react";
 import { Hike } from "./models";
 import { Marker } from "react-map-gl";
 import "@aws-amplify/ui-react-geo/styles.css";
+import { Grid, View, useTheme } from "@aws-amplify/ui-react";
 
 function App() {
   const [hikes, setHikes] = useState([]);
-
+  const { tokens } = useTheme();
   useEffect(() => {
     const getHikes = async () => {
-      try {
-        const hikes = await DataStore.query(Hike);
-        console.log(
-          " =-=-=--=-==-where is my fucking data=--==-=--==--==-=--=-= ",
-          hikes
-        );
-        setHikes(hikes);
-      } catch (error) {
-        console.log("Error retrieving posts", error);
-      }
+      const hikes = await DataStore.query(Hike);
+      setHikes(hikes);
     };
 
     getHikes();
   }, []);
-  console.log("useEffect -=-==--=", hikes);
   return (
-    <div className="App">
-      <HikingCollection />
-      <MapView
-        initialViewState={{
-          latitude: 33.743281,
-          longitude: -117.868986,
-          zoom: 7,
-        }}
-      >
-        {hikes.map((hike) => (
-          <Marker latitude={hike.lat} longitude={hike.long} key={hike.id} />
-        ))}
-      </MapView>
-    </div>
+    <Grid templateColumns="250px 1fr" templateRows="100vh">
+      <View backgroundColor={tokens.colors.blue[20]}></View>
+      <View>
+        <MapView
+          initialViewState={{
+            latitude: 33.743281,
+            longitude: -117.868986,
+            zoom: 7,
+          }}
+        >
+          {hikes.map((hike) => (
+            <Marker latitude={hike.lat} longitude={hike.long} key={hike.id} />
+          ))}
+        </MapView>
+      </View>
+    </Grid>
   );
 }
 
